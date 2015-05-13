bde6c3833c76ab467a5ab31ad3f89fded185e606
Zan Sullivan-Wilson <soonerfan237@gmail.com>
2015-05-12 23:23:16 -0500

    week 8 day 1

--- 11-express/11-exercises/blog/routes/posts.js
+++ 11-express/11-exercises/blog/routes/posts.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var Post = require('./../models/posts');
+
+// acquire an array of all posts
+var posts = Post.all();
+
+// acquire a single post, perhaps by a post id as a url param
+//
 
 router.get('/', function(req, res) {
-  res.send("display a list of all posts")
+  //res.send(posts);
+  res.render("index", {posts: posts})
 })
 
 router.get('/new', function(req, res) {
@@ -10,17 +18,21 @@ router.get('/new', function(req, res) {
 })
 
 router.post('/', function(req, res) {
-  res.send("create a new post")
+  res.render("new")
 })
 
 router.get('/:id', function(req, res) {
-  var id = req.params.id
-  res.send("display a specific post with id " + id)
+  var id = req.params.id;
+  var post = Post.find(id);
+  res.render("show", {id: post.id,
+                      title: post.title, 
+                      body: post.body,
+                      author: post.author})
 })
 
 router.get('/:id/edit', function(req, res) {
   var id = req.params.id
-  res.send("return an HTML form for editing a post with id " + id)
+  res.render("edit", post)
 })
 
 router.put('/:id', function(req, res) {
